725c360cf8226cb94bbaa84ef6a3fd8bedc64f24
Léa <lea.brechet@yahoo.fr>
2020-11-04 11:52:25 +0100

    create Player script + rb + box collider

---
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    [SerializeField] private float Speed;
+    [SerializeField] private float MaxSpeed;
+    
+    private Controls controls;
+    private Vector2 direction;
+
+    private void OnEnable()
+    {
+        controls = new Controls();
+        controls.Enable();
+
+        controls.Player.Move.performed += OnMovePerformed;
+        controls.Player.Move.canceled += OnMoveCanceled;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+    }
+
+    private void OnMovePerformed(InputAction.CallbackContext obj)
+    {
+        direction = obj.ReadValue<Vector2>();
+        Debug.Log(direction);
+    }
+
+    private void OnMoveCanceled(InputAction.CallbackContext obj)
+    {
+
+    }
+}
