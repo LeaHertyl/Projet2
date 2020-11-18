53b13780704dc52ec2fa51cc13e88bf8319dd4a5
LeaHertyl <lea.brechet@yahoo.fr>
2020-11-18 17:34:16 +0100

    new script with class who doesn't derive from Monobehaviour
    Derive to ScriptableObject

--- Assets/Scripts/PickUpItems.cs
+++ Assets/Scripts/PickUpItems.cs
@@ -7,5 +7,14 @@ public class PickUpItems : Interactable
     public override void Interact()
     {
         base.Interact();
+
+        PickUp();
+    }
+
+    private void PickUp()
+    {
+        Debug.Log("pickup");
+        //Place l'objet dans l'inventaire
+        Destroy(gameObject);
     }
 }
---
+++ Assets/Scripts/ScriptableObject.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scriptableobject : ScriptableObject
+{
+    //permet de créer une nouvelle classe de type ScriptableObject
+    //permet de créer des objets qui auront directement pour paramètre tout ce qu'on va écrire dans ce script
+    //sorte de modèle de base de pour créer des objets plus facilement/rapidement
+    //ci-dessous on va donc créer des paramètres de bases, vides ou nul par défaut, qu'on pourra modifier ensuite sur les différents objets créés
+    //va permettre de créer rapidement tous les items de nourriture
+
+    [SerializeField] private string Objectname = "New Item";
+    [SerializeField] private Sprite icon = null;
+
+}
