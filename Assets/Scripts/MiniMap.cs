d1c08655e100086cbccb3269eaa38ac2a1599219
LeaHertyl <lea.brechet@yahoo.fr>
2020-12-08 16:17:27 +0100

    Yey, I have a minimap

---
+++ Assets/Scripts/MiniMap.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniMap : MonoBehaviour
+{
+    [SerializeField] Transform Player;
+
+    private Vector3 newPosition;
+
+    private void LateUpdate()
+    {
+        newPosition = Player.position;
+        newPosition.y = transform.position.y;
+        transform.position = newPosition;
+
+        transform.rotation = Quaternion.Euler(90f, Player.eulerAngles.y, 0f);
+    }
+}
