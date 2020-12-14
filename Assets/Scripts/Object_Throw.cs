f3b43c7a1e59792aee935098c6abfd10cc283e8f
LeaHertyl <lea.brechet@yahoo.fr>
2020-12-14 14:23:57 +0100

    creating particules for hill items -> prefab

--- Assets/Scripts/Object_Throw.cs
+++ Assets/Scripts/Object_Throw.cs
@@ -43,17 +43,18 @@ public class Object_Throw : MonoBehaviour
         if(transform.position.y <= -20)
         {
             Destroy(gameObject); //on detruit le gameObject associe a ce script -> permet de supprimer les objets quand ils ont ete lance et qu'on ne les voit plus dans la scene et qu'ils ne tombent pas a l'infini
-            Destroy(ExplosionToDestroy);
+            Destroy(ExplosionToDestroy); //on detruit le gameObject correspondant aux particules d'explosion pour qu'elles se détruisent en même temps que l'objet
         }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        Explod();
+        Explod(); //on appelle la fonction Explod() quand l'objet en trigger un autre
     }
 
     private void Explod()
     {
-        ExplosionToDestroy = Instantiate(ExplosionEffect, transform.position, transform.rotation);
+        //on stock l'objet qu'on va instancier dans une variable pour pouvoir l'appeler et y faire reference ailleurs
+        ExplosionToDestroy = Instantiate(ExplosionEffect, transform.position, transform.rotation); //on instancie l'effet de particules d'explosion au même endroit que l'objet auquel ce script est associe
     }
 }
