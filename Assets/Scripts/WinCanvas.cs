2a03fc897387341c8fbe268031187d28057b9a4c
LeaHertyl <lea.brechet@yahoo.fr>
2020-12-15 00:21:33 +0100

    Win condition ok
    Il faut encore que j'arrive à afficher le bon nom de joueur -> est-ce que faut que je fasse deux scène en fonction de qui gagne ?

---
+++ Assets/Scripts/WinCanvas.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class WinCanvas : MonoBehaviour
+{
+    [SerializeField] GameObject Winner;
+    [SerializeField] GameObject Loser;
+
+    private string winner;
+    private string loser;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var winnerf = Winner.GetComponent<TextMeshProUGUI>();
+        winner = winnerf.text;
+
+        var loserf = Loser.GetComponent<TextMeshPro>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var Player1 = GameObject.FindWithTag("Player1");
+        var Player1Script = Player1.GetComponent<PlayerBehaviour>();
+
+        var Player1currentHealth = Player1Script.currentHealth1;
+
+        var Player2 = GameObject.FindWithTag("Player2");
+        var Player2Script = Player2.GetComponent<Player2Behaviour>();
+
+        var Player2currentHealth = Player2Script.currentHealth2;
+
+        if (Player1currentHealth == 0)
+        {
+            winner = "Player 2";
+        }
+    }
+}
---
+++ Assets/Scripts/WinCondition.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinCondition : MonoBehaviour
+{
+    //[SerializeField] Scene EndScene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var Player1 = GameObject.FindWithTag("Player1");
+        var Player1Script = Player1.GetComponent<PlayerBehaviour>();
+
+        var Player1currentHealth = Player1Script.currentHealth1;
+
+        var Player2 = GameObject.FindWithTag("Player2");
+        var Player2Script = Player2.GetComponent<Player2Behaviour>();
+
+        var Player2currentHealth = Player2Script.currentHealth2;
+
+        if(Player1currentHealth == 0 || Player2currentHealth == 0)
+        {
+            SceneManager.LoadScene("EndScene");
+        }
+    }
+}
