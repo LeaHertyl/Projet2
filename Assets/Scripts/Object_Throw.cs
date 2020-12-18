21a70d446a11ae8866531d6d24e99e45c755d616
LeaHertyl <lea.brechet@yahoo.fr>
2020-12-18 09:58:03 +0100

    Fixing the trow bug !

--- Assets/Scripts/Object_Throw.cs
+++ Assets/Scripts/Object_Throw.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class Object_Throw : MonoBehaviour
 {
     [SerializeField] private float throwForce;
+    [SerializeField] private float radius;
     [SerializeField] private GameObject ExplosionEffect;
 
     private bool isthrowing; //on cree un booleen
     private bool isthrowing2;
+    private float distance1;
+    private float distance2;
     private Rigidbody myRB; //on cree une variable de type Rigibody
     private GameObject ExplosionToDestroy;
 
@@ -26,6 +29,7 @@ public class Object_Throw : MonoBehaviour
         //on recupere le script PlayerBehaviour qui se trouve sur le GameObject ayant le tag Player 1
         var Player1 = GameObject.FindWithTag("Player1");
         var Player1Script = Player1.GetComponent<PlayerBehaviour>();
+        var Player1Transform = Player1.GetComponent<Transform>();
 
         //on recupere le composant Transform qui se trouve sur le GameObject ayant le tag Camera1
         var Player1Camera = GameObject.FindWithTag("Camera1");
@@ -36,6 +40,7 @@ public class Object_Throw : MonoBehaviour
         //on recupere le script PlayerBehaviour qui se trouve sur le GameObject ayant le tag Player 1
         var Player2 = GameObject.FindWithTag("Player2");
         var Player2Script = Player2.GetComponent<Player2Behaviour>();
+        var Player2Transform = Player2.GetComponent<Transform>();
 
         //on recupere le composant Transform qui se trouve sur le GameObject ayant le tag Camera1
         var Player2Camera = GameObject.FindWithTag("Camera2");
@@ -43,14 +48,17 @@ public class Object_Throw : MonoBehaviour
 
         isthrowing2 = Player2Script.isThrowing; //on associe la valeur de la variable isThrowing du script PlayerBehaviour a la varibale isthrowing
 
+        distance1 = Vector3.Distance(Player1Transform.position, transform.position); //verifie la distance entre le player et l'objet a ramasser
+        distance2 = Vector3.Distance(Player2Transform.position, transform.position); //verifie la distance entre le player et l'objet a ramasser
+
         //si isthrowing == true -> si isThrowing == true -> si le Player a enclenche l'input pour lancer les objets et qu'il avait un objet dans la main
-        if (isthrowing)
+        if (isthrowing && distance1 < distance2)
         {
             myRB.useGravity = true; //on active la gravite sur le composant Rigidbody de l'objet -> pour qu'il ait une physique et retombe apres avoir ete lance
             myRB.AddForce(Camera1Transform.forward * throwForce); //on ajoute une force a l'objet en direction de la ou regarde la camera pour le lancer devant le Player selon une force definie
         }
 
-        if(isthrowing2)
+        if(isthrowing2 && distance2 < distance1)
         {
             myRB.useGravity = true;
             myRB.AddForce(Camera2Transform.forward * throwForce);
@@ -73,5 +81,11 @@ public class Object_Throw : MonoBehaviour
     {
         //on stock l'objet qu'on va instancier dans une variable pour pouvoir l'appeler et y faire reference ailleurs
         ExplosionToDestroy = Instantiate(ExplosionEffect, transform.position, transform.rotation); //on instancie l'effet de particules d'explosion au même endroit que l'objet auquel ce script est associe
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, radius);
     }
 }
